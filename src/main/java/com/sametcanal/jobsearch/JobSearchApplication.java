77d1689c1fec82bc8ec3bbabcf0500bae572bc8d
Sametcan AL <sametcanal53@hotmail.com>
2023-03-08 10:50:29 +0300

    Database Configuration and Security inactive

--- src/main/java/com/sametcanal/jobsearch/JobSearchApplication.java
+++ src/main/java/com/sametcanal/jobsearch/JobSearchApplication.java
@@ -3,7 +3,12 @@ package com.sametcanal.jobsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//Security inactive
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+/*@SpringBootApplication()*/
 public class JobSearchApplication {
 
     public static void main(String[] args) {
