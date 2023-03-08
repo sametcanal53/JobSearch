85e1903b8b85fec212b8529cd8b83a36c32fcd26
Sametcan AL <sametcanal53@hotmail.com>
2023-03-08 10:34:21 +0300

    Spring Initialize

---
+++ src/main/java/com/sametcanal/jobsearch/JobSearchApplication.java
@@ -0,0 +1,13 @@
+package com.sametcanal.jobsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sametcanal/jobsearch/JobSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sametcanal.jobsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
